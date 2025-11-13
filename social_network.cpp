60aa27beab282b126bd8f0bda4adba367ece3b4f
kartik08 <kartikmeena925@gmail.com>
2025-11-14 01:06:03 +0530

    Update social_network.cpp
    added user class and screen clear functions

--- social_network.cpp
+++ social_network.cpp
@@ -10,3 +10,42 @@
 #include <chrono>
 #include <cctype>
 #include <unistd.h>
+
+using namespace std;
+
+void clearScreen()
+{
+  #ifdef _WIN32
+system("cls");
+#else
+system("clear");
+#endif
+}
+
+void waitAndClear()
+{
+  this_thread::sleep_for(std::chrono::milliseconds(900));
+clearScreen();
+}
+
+class User //defining user
+{
+public:
+string name;
+string username;
+string dob;
+string gender;
+
+User() {}
+
+User(string n,string u, string d,string g)
+: name(n),username(u),dob(d),gender(g) {}
+
+void display() const
+{
+  cout << "Name: " << name << endl;
+  cout << "Username: @" << username << endl;
+  cout << "DOB: " << dob << endl;
+cout << "Gender: " << gender << endl;
+}
+};
